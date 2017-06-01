2aa1ed0e08bdce5229ca0f8bc0d262b41f6a8c44
makerdiary <zelin@makerdiary.co>
2017-06-01 23:32:03 +0800

    add BLE Services

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-var led = new DigitalOut(LED1);
-var iv = setInterval(function() {
-  led.write(led.read() ? 0 : 1);
-}, 500);
 
-print("To stop LED1 from blinking, call `clearInterval(" + iv + ")`");
 
 var repl = new JSRepl();
